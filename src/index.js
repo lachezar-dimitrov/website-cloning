72590ca2704cfa41583c7d035ca18e390b2dc67f
lachezar-dimitrov <lachezar_dimitrov.outlook.com>
2023-05-29 19:54:33 +0300

    Create the root of our project

--- src/index.js
+++ src/index.js
@@ -1,5 +1,34 @@
-const _ = require('lowdash');
-const arr = Array.from({ length: 5 });
-_.f
-console.log(arr);
+const navigationItems = [
+  {
+    title: "Home",
+    url: "/",
+  },
+  {
+    title: "About",
+    url: "/about",
+  },
+  {
+    title: "Contact",
+    url: "/contact",
+  },
+];
 
+const root = document.getElementById("root");
+const navigation = document.createElement("nav");
+const navigationList = document.createElement("ul");
+
+root.appendChild(navigation);
+navigation.appendChild(navigationList);
+
+for (const navigationItem of navigationItems) {
+  const navigationLink = document.createElement("a");
+
+  navigationLink.href = navigationItem.url;
+  navigationLink.textContent = navigationItem.title;
+
+  navigationLink.classList.add("navigation__link");
+
+  navigationList.appendChild(navigationLink);
+}
+
+console.log({ root });
