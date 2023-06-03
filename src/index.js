75bde1eb539388570961f7ba23036851f1772220
tihomir-petrovv <118721@students.ue-varna.bg>
2023-06-03 17:11:54 +0300

    Changing burgerInfo

--- src/index.js
+++ src/index.js
@@ -34,9 +34,9 @@ for (const navigationItem of navigationItems) {
 const ingredientRemove = "ingredientRemove"
 const burgerMenus = document.createElement("div");
 burgerMenus.classList.add("burgers")
-const burgerBeggining= document.createElement("div");
+const burgerBeggining = document.createElement("div");
 burgerBeggining.classList.add("burgers")
-burgerBeggining.setAttribute("id" , ingredientRemove)
+burgerBeggining.setAttribute("id", ingredientRemove)
 const burgerTop = document.createElement("img");
 burgerTop.src = "Images/burgerTop.jfif"
 const burgerBottom = document.createElement("img");
@@ -46,18 +46,21 @@ burgerMenus.appendChild(burgerBeggining)
 burgerMenus.appendChild(burgerBottom)
 const add1 = document.createElement("button")
 add1.classList.add("buttons")
+add1.setAttribute("id", 'saladButton')
 add1.textContent = "Добавете салата";
-add1.addEventListener('click',() => addIngredients1("salad","Images/salad.jpg"))
+add1.addEventListener("click", () => addIngredients1("salad", "Images/salad.jpg", "salad2"))
 const add2 = document.createElement("button")
 add2.classList.add("buttons")
+add2.setAttribute("id", 'meatButton')
 add2.textContent = "Добавете месо";
-add2.addEventListener('click',() => addIngredients1("meat","Images/meat.jpg"))
+add2.addEventListener("click", () => addIngredients1("meat", "Images/meat.jpg", "meat2"))
 const add3 = document.createElement("button")
 add3.classList.add("buttons")
 add3.textContent = "Добавете сирене";
-add3.addEventListener('click',() => addIngredients1("cheese","Images/cheese.jpg"))
+add3.setAttribute("id", 'cheeseButton')
+add3.addEventListener("click", () => addIngredients1("cheese", "Images/cheese.jpg", "cheese2"))
 const add4 = document.createElement("button")
-add4.addEventListener('click', () => addToBasket())
+add4.addEventListener("click", () => addToBasket())
 add4.classList.add("buttons")
 add4.textContent = "Добави в количка";
 
@@ -76,42 +79,71 @@ const normalPrice = document.createElement("div")
 normalPrice.textContent = "Цена: 5лв."
 Price.appendChild(normalPrice)
 Price.appendChild(add5)
+let storeNumberofBurgers = 0
+const numberOfIngrediensandBurgers = document.createElement("h1")
+const basketContainer = document.createElement("div")
+Price.appendChild(numberOfIngrediensandBurgers)
+Price.appendChild(basketContainer)
 
 const ingredientCount = {
   salad: 0,
   meat: 0,
   cheese: 0
 }
-
-let awer = 5
-const cena = "Цена: "
-function addIngredients1(ingredientType,src){
-    const ingredientPosition = document.createElement("img")
-    ingredientPosition.src = src
-    ingredientPosition.style.Top = "40px"
-    burgerBeggining.appendChild(ingredientPosition)
-    ingredientCount[ingredientType]+=1
-    if(ingredientCount[ingredientType] > 1){
-        awer++ 
-        normalPrice.textContent = cena + awer + "лв."
-        }
-      
+const ingredientPrice = {
+  salad2: 0.5,
+  meat2: 2.5,
+  cheese2: 1.5
 }
 
-let storeNumberofBurgers = 0
+let stockPrice = 5
+const price = "Цена: "
+function addIngredients1(ingredientType, src, ingredients) {
+  const ingredientPosition = document.createElement("img")
+  ingredientPosition.src = src
+  burgerBeggining.appendChild(ingredientPosition)
+  ingredientCount[ingredientType] += 1
+  if (ingredientCount[ingredientType] > 1) {
+    stockPrice += ingredientPrice[ingredients]
+    normalPrice.textContent = price + stockPrice + "лв."
+  }
+  
+}
 
-function addToBasket(){
+
+function addToBasket() {
   normalPrice.textContent = "Цена: 5лв."
   const deletingIngredient = document.getElementById(ingredientRemove)
   deletingIngredient.innerHTML = null
   storeNumberofBurgers++
   add5.textContent = storeNumberofBurgers + " Бургера в кошницата"
-    
-    
 
+
+  let entries = Object.entries(ingredientCount);
+  const ingredientsList = document.createElement("ul")
+  for (const ingredientInformation of entries) {
+    const [ingredientName, ingredientCount1] = ingredientInformation
+    addIngredientInfo(ingredientName, ingredientCount1, ingredientsList)
+
+  }
+  numberOfIngrediensandBurgers.textContent = `${storeNumberofBurgers} бургера`
+  basketContainer.appendChild(ingredientsList)
+  resetBurgerBuilder()
+}
+function resetBurgerBuilder() {
+  for (let placeOfingredient in ingredientCount) {
+    if (ingredientCount.hasOwnProperty(placeOfingredient)) {
+      ingredientCount[placeOfingredient] = 0;
+    }
+  }
+  stockPrice = 5
 }
 
-
+function addIngredientInfo(ingredientName, ingredientCount, parentNode) {
+  const listItem = document.createElement("li")
+  listItem.textContent = `${ingredientCount}: ${ingredientName}`
+  parentNode.appendChild(listItem)
+}
 
 root.appendChild(Price)
 console.log({ root });
